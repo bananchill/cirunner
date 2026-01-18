833a892e266e926e572c18ea8ff1b917c213dadd
«Maksim <maksim070601@gmail.com>
2026-01-18 15:42:37 +0300

    add github actions

--- apps/backend/.eslintrc.cjs
+++ apps/backend/.eslintrc.cjs
@@ -1,11 +1,7 @@
 module.exports = {
-  root: true,
-  env: { node: true, es2022: true },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module",
-  },
+    root: true,
+    env: { node: true, es2022: true },
+    extends: ['../../.eslintrc.js'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
 };
+
